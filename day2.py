865aa5ee7738e8cb1c986d00e26e531f3bceb9ce
keshav7-ops <85719153+keshav7-ops@users.noreply.github.com>
2021-06-11 16:23:13 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,28 @@
+#this is a program to print values of different variables having different data types using .format function
+
+
+name = 'keshav'
+a = "my name is {}".format(name)             
+age = 20
+b = "i am {} years old".format(age)
+state = 'haryana'
+c = "i live in {}".format(state)
+print(a)
+print(b)
+print(c)
+
+
+'''
+or we can use another method to print values of different variables having different data types using .format function
+
+name = 'keshav'
+a = f"my name is {name}".format()             
+age = 20
+b = f"i am {age} years old".format()
+state = 'haryana'
+c = f"i live in {state}".format()
+print(a)
+print(b)
+print(c)
+
+'''
