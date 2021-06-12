9b51171239e96457bf89f77e7d9c758f9990e01c
keshav7-ops <85719153+keshav7-ops@users.noreply.github.com>
2021-06-12 13:03:00 +0530

    Add files via upload

---
+++ age_calculator.py
@@ -0,0 +1,3 @@
+end_year = 2020
+birth_year = int(input('Enter birth year:'))
+print("Your age is :",end_year-birth_year)
---
+++ simple_calculator.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))          
+b = int(input("Enter second number: "))
+print("Sum is :",a+b)
+print("Difference is :",a-b)
+print("Multiplication is :",a*b)
+print("Division is :",a/b)
+print("Decimal division is :",a//b)             #this operator simply gives the quotient in integer form
+print("Remainder is :",a%b)
+print("a power b :",a**b)
